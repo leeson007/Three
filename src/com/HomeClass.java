958c3b60e255562f70a7197c02a8f3d382f48fad
leeson_home <leesongit@gmail.com>
2016-08-04 22:14:23 +0800

    import jar

--- src/com/HomeClass.java
+++ src/com/HomeClass.java
@@ -1,5 +1,14 @@
 package com;
 
-public class HomeClass {
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
+
+public class HomeClass {
+	@Test
+	public void test(){
+		WebDriver driver=new FirefoxDriver();
+		driver.get("http://www.baidu.com");
+	}
 }
